fd81bb90e7dae21861cc0cf6c520b8ea25e4b1c6
MohammedAli-Biar <maab16@alu.ua.es>
2025-05-21 17:10:14 +0200

    error en pull

--- Base_de_datos/__init__.py
+++
@@ -1,5 +0,0 @@
-from .guardar_partida import GuardarPartida
-from .operacoines_pandas import *
-from .operaciones_sqlite import *
-
-__all__ = ["guardar_partida","operacoines_pandas","operaciones_sqlite"]
--- Partidas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Partida import partida
-from .aflabeto_FEN import *
-from .error_partidas import ErrorPartida
-
-__all__ = ["Partida", "aflabeto_FEN", "error_partidas"]
